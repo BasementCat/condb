f92a53e135840fd75883a94730fa3341e7639cd5
Alec Elton <alec.elton@gmail.com>
2016-09-14 18:58:49 -0500

    Filters, hide some fields from edit view

--- app/views/__init__.py
+++ app/views/__init__.py
@@ -23,12 +23,22 @@ class CustomModelView(ModelView):
     def column_exclude_list(self):
         return ['created_at', 'updated_at']
 
+    @property
+    def column_filters(self):
+        return []
+
     def is_accessible(self):
         return current_user.is_authenticated
 
     def inaccessible_callback(self, name, **kwargs):
         # redirect to login page if user doesn't have access
         return redirect(url_for('user.login', next=request.url))
+
+
+class HiddenModelViewMixin(object):
+    @property
+    def column_filters(self):
+        return super(HiddenModelViewMixin, self).column_filters + ['is_hidden']
 
 
 class EditorModelView(CustomModelView):
@@ -41,8 +51,16 @@ class AdminModelView(CustomModelView):
         return super(AdminModelView, self).is_accessible() and current_user.has_roles('admin')
 
 
-class ConventionLocationModelView(EditorModelView):
-    form_excluded_columns = ['distances']
+class ConventionLocationModelView(HiddenModelViewMixin, EditorModelView):
+    form_excluded_columns = ['distances', 'convention_years']
+
+    @property
+    def column_filters(self):
+        return super(HiddenModelViewMixin, self).column_filters + [
+            'country_code',
+            'state_province',
+            'city',
+        ]
 
     def on_model_change(self, form, model, is_created=False):
         with db.session.no_autoflush:
@@ -61,10 +79,26 @@ class RoleModelView(AdminModelView):
     form_excluded_columns = ['users']
 
 
+class ConventionTypeModelView(HiddenModelViewMixin, EditorModelView):
+    form_excluded_columns = ['conventions']
+
+
+class ConventionThemeModelView(HiddenModelViewMixin, EditorModelView):
+    form_excluded_columns = ['convention_years']
+
+
+class ConventionModelView(HiddenModelViewMixin, EditorModelView):
+    form_excluded_columns = ['years']
+
+
+class ConventionYearModelView(HiddenModelViewMixin, EditorModelView):
+    pass
+
+
 admin.add_view(UserModelView(models.User, db.session))
 admin.add_view(RoleModelView(models.Role, db.session))
-admin.add_view(EditorModelView(models.ConventionType, db.session))
-admin.add_view(EditorModelView(models.ConventionTheme, db.session))
+admin.add_view(ConventionTypeModelView(models.ConventionType, db.session))
+admin.add_view(ConventionThemeModelView(models.ConventionTheme, db.session))
 admin.add_view(ConventionLocationModelView(models.ConventionLocation, db.session))
-admin.add_view(EditorModelView(models.Convention, db.session))
-admin.add_view(EditorModelView(models.ConventionYear, db.session))
+admin.add_view(ConventionModelView(models.Convention, db.session))
+admin.add_view(ConventionYearModelView(models.ConventionYear, db.session))
